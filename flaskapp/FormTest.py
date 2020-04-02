640a2b74d9750a9020110745f63dccb70a8f1d22
Nina Ahmed <ninaahmed99@gmail.com>
2020-04-01 22:47:25 -0500

    Can remove student from queue, start instructor login code

--- flaskapp/FormTest.py
+++ flaskapp/FormTest.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, Email
 
 class EnterLineForm(FlaskForm):
     name = StringField('Name', 
-            validators=[DataRequired(), Length(min=2, max=30)])
+        validators=[DataRequired(), Length(min=2, max=30)])
     email = StringField('Email',
-            validators=[DataRequired(), Email()])
+        validators=[DataRequired(), Email()])
     eid = StringField('EID',
-            validators=[DataRequired()])
+        validators=[DataRequired()])
     submit = SubmitField('Join the Queue!')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+        validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+        validators=[DataRequired()])
+    submit = SubmitField('Sign In')
