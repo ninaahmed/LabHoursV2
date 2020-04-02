9890d3d8cfb9d8693a8934cc7ab4294f8af82b17
Nina Ahmed <ninaahmed99@gmail.com>
2020-04-02 02:05:12 -0500

    Students can remove themselves! Added page

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -71,6 +71,16 @@ def login():
         print(f"not validated errors={form.errors}")
     return render_template('login.html', title='Sign In', form=form)
 
+@app.route('/remove', methods=['GET', 'POST'])
+def remove_student():
+    message = ""
+    if request.method == 'POST':
+        if queue_handler.remove_eid(request.form['eid']):
+            return redirect(url_for('view_line'))
+        else:
+            message = "Sorry that didn't work out :-/"
+    return render_template('remove.html', message=message)
+
 @app.route('/logout')
 def logout():
     logout_user()
