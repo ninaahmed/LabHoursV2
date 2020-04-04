e6f26ed2767df5cf873ac033ae1cd11f2b386f07
Nina Ahmed <ninaahmed99@gmail.com>
2020-04-03 19:36:55 -0500

    Fixed block wrapping

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -41,7 +41,9 @@ def join():
             place = queue_handler.enqueue(s)
             flash(f'{form.name.data} has been added to the queue!', 'success')
             try:
-                notifier.send_message(form.email.data, "Notification from 314 Lab Hours Queue", render_template("added_to_queue_email.html", place_str=get_place_str(place), student_name=form.name.data, remove_code="312-314"), 'html')
+                notifier.send_message(form.email.data, "Notification from 314 Lab Hours Queue", 
+                render_template("added_to_queue_email.html", place_str=get_place_str(place), 
+                student_name=form.name.data, remove_code="312-314"), 'html')
             except Exception as e:
                 print(f"Failed to send email to {form.email.data}\n{e}")
             return redirect(url_for('view_line'))
@@ -117,65 +119,3 @@ def change_zoom():
 def logout():
     logout_user()
     return redirect(url_for('join'))
-
-"""
-    Formats a place in the queue with the appropriate suffix.
-    i.e. 1 => "1st", 2 -> "2nd", etc
-    Used in the email when someone joins the queue
-"""
-def get_place_str(place):
-    place_str = str(place)
-    if len(place_str) == 2 and place_str[0] == '1':
-        return f"{place}th"
-    elif place_str[-1] == '1':
-        return f"{place}st"
-    elif place_str[-1] == '2':
-        return f"{place}nd"
-    elif place_str[-1] == '3':
-        return f"{place}rd"
-    else:
-        return f"{place}th"
-
-"""
-    Handle post requests in the view line page.
-"""
-def handle_line_form(request):
-    global queue_is_open
-    # Handle removing student
-    if 'finished' in request.form or 'removed' in request.form:
-        handle_remove(request)
-    elif 'close' in request.form:
-        queue_is_open = False
-    elif 'open' in request.form:
-        queue_is_open = True
-        
-"""
-    Handles removing a student from the view queue
-    page. This can be done either using the "Finish"
-    button once a student is helped or the "Remove" button
-    if a student is removed from the queue without being
-    helped. Will notify new runner-up in the queue.
-"""
-def handle_remove(request):
-    if 'finished' in request.form:
-        uid = request.form['finished']
-    elif 'removed' in request.form:
-        uid = request.form['removed']
-    queue_handler.remove(uid)
-    # Update visit entry in the database
-    v = Visit.query.filter_by(id=uid).first()
-    v.time_left = datetime.utcnow()
-    if 'finished' in request.form:
-        v.was_helped = 1
-        v.instructor_id = current_user.id
-    elif 'removed' in request.form:
-        v.was_helped = 0
-    # Write changes to database
-    db.session.commit()
-    # Notify runner-up in the queue
-    s = queue_handler.peek_runner_up()
-    if s is not None:
-        try:
-            notifier.send_message(s.email, "Notification from 314 Lab Hours Queue", render_template("up_next_email.html", student_name=s.name, remove_code="312-314"), 'html')
-        except:
-            print(f"Failed to send email to {s.email}")
---
+++ flaskapp/routes_helper.py
@@ -0,0 +1,62 @@
+"""
+    Handles removing a student from the view queue
+    page. This can be done either using the "Finish"
+    button once a student is helped or the "Remove" button
+    if a student is removed from the queue without being
+    helped. Will notify new runner-up in the queue.
+"""
+def handle_remove(request):
+    if 'finished' in request.form:
+        uid = request.form['finished']
+    elif 'removed' in request.form:
+        uid = request.form['removed']
+    queue_handler.remove(uid)
+    # Update visit entry in the database
+    v = Visit.query.filter_by(id=uid).first()
+    v.time_left = datetime.utcnow()
+    if 'finished' in request.form:
+        v.was_helped = 1
+        v.instructor_id = current_user.id
+    elif 'removed' in request.form:
+        v.was_helped = 0
+    # Write changes to database
+    db.session.commit()
+    # Notify runner-up in the queue
+    s = queue_handler.peek_runner_up()
+    if s is not None:
+        try:
+            notifier.send_message(s.email, "Notification from 314 Lab Hours Queue", 
+            render_template("up_next_email.html", student_name=s.name, remove_code="312-314"), 'html')
+        except:
+            print(f"Failed to send email to {s.email}")
+
+"""
+    Formats a place in the queue with the appropriate suffix.
+    i.e. 1 => "1st", 2 -> "2nd", etc
+    Used in the email when someone joins the queue
+"""
+def get_place_str(place):
+    place_str = str(place)
+    if len(place_str) == 2 and place_str[0] == '1':
+        return f"{place}th"
+    elif place_str[-1] == '1':
+        return f"{place}st"
+    elif place_str[-1] == '2':
+        return f"{place}nd"
+    elif place_str[-1] == '3':
+        return f"{place}rd"
+    else:
+        return f"{place}th"
+
+"""
+    Handle post requests in the view line page.
+"""
+def handle_line_form(request):
+    # Handle removing student
+    if 'finished' in request.form or 'removed' in request.form:
+        handle_remove(request)
+        return True
+    elif 'close' in request.form:
+        return False
+    elif 'open' in request.form:
+        return True
